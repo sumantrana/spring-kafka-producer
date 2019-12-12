f099b5f73a03d300d418f8bd336d0aea7b8db638
sumantrana <srana@pivotal.io>
2019-12-12 09:01:11 +0900

    Added kafka exception handler

--- src/main/java/com/sumant/boot/learning/springkafkaproducer/KafkaBookProducer.java
+++ src/main/java/com/sumant/boot/learning/springkafkaproducer/KafkaBookProducer.java
@@ -1,9 +1,20 @@
 package com.sumant.boot.learning.springkafkaproducer;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.SerializationUtils;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 @Component
 public class KafkaBookProducer {
@@ -13,13 +24,32 @@ public class KafkaBookProducer {
 
     private KafkaTemplate<String, Book> kafkaTemplate;
 
+    private BlockingQueue<Book> failedMessageQueue = new ArrayBlockingQueue<Book>(5);
+
     KafkaBookProducer(KafkaTemplate<String, Book> kafkaTemplate){
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void sendBookMessage(Book book){
         ProducerRecord<String, Book> producerRecord = new ProducerRecord<String, Book>(bookTopic, book);
-        kafkaTemplate.send(producerRecord);
+        producerRecord.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, "kReplies".getBytes()));
+
+        ListenableFuture<SendResult<String, Book>> future = kafkaTemplate.send(producerRecord);
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Book>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, Book> result) {
+                System.out.println("Message sent successfully");
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Exception while sending message " + ex);
+            }
+
+        });
+
         kafkaTemplate.flush();
     }
+
 }
--- src/main/java/com/sumant/boot/learning/springkafkaproducer/SpringKafkaProducerApplication.java
+++ src/main/java/com/sumant/boot/learning/springkafkaproducer/SpringKafkaProducerApplication.java
@@ -1,13 +1,27 @@
 package com.sumant.boot.learning.springkafkaproducer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringKafkaProducerApplication {
+public class SpringKafkaProducerApplication implements CommandLineRunner {
+
+    @Autowired
+    KafkaBookProducer kafkaBookProducer;
+
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringKafkaProducerApplication.class, args);
+
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Book book = Book.builder().name("Sumant").value(10).build();
+        kafkaBookProducer.sendBookMessage(book);
+        kafkaBookProducer.sendBookMessage(book);
+    }
 }
